16bafc51847d103b5068ca750d83dc3e91ad694f
Gaja <gaja@minn.lt>
2024-10-29 13:00:51 +0100

    Interactable class building from UML

---
+++ WorldOfZuul/Interactable.cs
@@ -0,0 +1,29 @@
+namespace WorldOfZuul
+{
+
+public class Interactable
+{
+    public string Name {get; set; }
+    public Room ParentRoom {get; set; }
+    public List<Event> Events {get; set; } 
+    protected Game gameRef; 
+    public Interactable(Game game)
+    {
+        gameRef = game;              // so the main game would know what is happening, e.g., so it would know that the player found a key
+        Name = name;
+        Events = new List<Events>(); // if you don't initialise a list, you can't put anything into it
+                                     // we want, e.g., this not to crash: Events.Add(newEvent)
+    }
+
+    public virtual void Interact()   // this is what happens when the player in the game interacts with the interactable
+    {
+        Console.WriteLine($"Interacting with {Name}");
+    }
+}
+
+// example child class:
+public class Merchant : Interactable
+{
+    
+}
+}
