58f99102f688a0508ad2048162ed0b2dfa62c0e6
Gaja <gaja@minn.lt>
2024-11-05 15:27:28 +0100

    status bar (available through a command)

--- WorldOfZuul/Player.cs
+++ WorldOfZuul/Player.cs
@@ -54,17 +54,17 @@ public class Player{
 /*
 string statBar = "Social: " + Resources[Social] + " | Environment: " + Resources[Environment] + "
 | Money: $" Resources[Money] ?;
-Console.SetCursorPosition(0,1);
+Console.SetCursorPosition(0,0);
 Console.WriteLine(statBar);
 
 
 probably in the Game class, method:
 
-UpdateStatBar(string Player)
+UpdateStatBar(string something)
 {
-Player.
+Player.something
 }
-- and this method might need to be called to events because that's where after picking from
+- and this method might need to be called to player because that's where after picking from
 multiple choices, you might increase or reduce your resources
 
 
